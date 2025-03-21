96b049685ddd549f451ac238e0d1b738a291f4d2
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2025-03-21 13:41:36 +0100

    week 2 / 2: functions

---
+++ 02/02_functions.py
@@ -0,0 +1,8 @@
+# define a function that returns 42
+def return_42():
+    return 42
+
+
+# create a function called my_function that takes two arguments and returns the product of these arguments
+def my_function(x,y):
+    return x*y
