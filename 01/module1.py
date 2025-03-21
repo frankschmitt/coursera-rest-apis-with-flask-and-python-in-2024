12ea8ec6d861f005d79992694687fc06a4d130fa
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2025-03-21 13:17:04 +0100

    added week 01

---
+++ 01/module1.py
@@ -0,0 +1,8 @@
+# create two variables var1 and var2 both with the same value
+var1 = 4
+var2 = 4
+
+# create two variables num1 and num2 whose product is 16
+num1 = 3.2
+num2 = 5
+print(num1*num2)
---
+++ 01/module2.py
@@ -0,0 +1,20 @@
+# create a list whose sum is 100
+my_list = [10, 40, 50]
+sum(my_list)
+
+# define a tuple with one element
+my_tuple = ('x') # wrong
+
+my_tuple = ('x',) # notice the comma
+
+# given
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5}
+set1.intersection(set2)
+
+# modify set2 so that set1.intersection(set2) returns {9, 77, 12, 5}
+
+
+set2 = {5, 77, 9, 12, 1313}
+set1.intersection(set2)
+
