1a382c2542055cd9e604eee2681033d21331f78f
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2025-03-26 13:40:14 +0100

    week 2 / 3: dictionaries + students: solution

--- 02/03_dictionaries-and-students.py
+++ 02/03_dictionaries-and-students.py
@@ -1,13 +1,13 @@
 # Create a variable called student with a dictionary.
-# Must contain three keys: nmae, school and grades
+# Must contain three keys: name, school and grades
 # Values should be Jose, Computing and a tuple with values 66, 77 and 88
-student = 
+student = { 'name': 'Jose', 'school': 'Computing', 'grades': (66, 77, 88) }
 
 
 # Assume the target (data) is a dictionary
 # Modify the grades variable so it accesses the 'grades' key of the data dictionary
 def average_grade(data):
-    grades = #change this
+    grades = data['grades'] #change this
     return sum(grades) / len(grades)
 
 
@@ -16,5 +16,16 @@ def average_grade(data):
 # You must add all grades of all students together
 # You must also count how many grades there are in total in the entire list
 def average_grade_all_students(student_list):
-    total = 0
-    ...
+    all_grades = []
+    for s in student_list:
+        all_grades += s['grades']
+    return sum(all_grades) / len(all_grades)
+
+
+if __name__ == '__main__':
+    jose = { 'name': 'Jose', 'school': 'Computing', 'grades': (66, 77, 88) }
+    valim = { 'name': 'Valim', 'school': 'Elixir', 'grades': (99, 110, 121) }
+    print(f"jose: {average_grade(jose)}")
+    student_list = [jose, valim ]
+                    
+    print(f"input: {student_list}, avg: {average_grade_all_students(student_list)}")
