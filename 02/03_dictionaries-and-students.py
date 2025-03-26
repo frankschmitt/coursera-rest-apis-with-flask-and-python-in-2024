5d373f8cdd9bba8b72f3349fd4dba3e81c65e7c7
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2025-03-26 13:28:08 +0100

    week 2 / 3: dictionaries + students: setup

---
+++ 02/03_dictionaries-and-students.py
@@ -0,0 +1,20 @@
+# Create a variable called student with a dictionary.
+# Must contain three keys: nmae, school and grades
+# Values should be Jose, Computing and a tuple with values 66, 77 and 88
+student = 
+
+
+# Assume the target (data) is a dictionary
+# Modify the grades variable so it accesses the 'grades' key of the data dictionary
+def average_grade(data):
+    grades = #change this
+    return sum(grades) / len(grades)
+
+
+# implement the function below
+# Given a list of students (dictionaries), calculate the average grade of the class
+# You must add all grades of all students together
+# You must also count how many grades there are in total in the entire list
+def average_grade_all_students(student_list):
+    total = 0
+    ...
