c91c827984583b4b44e9a844e58958a39b56dd79
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2025-04-15 08:47:43 +0200

    week 2 / 4: classes + objects: setup

---
+++ 02/04_classes-and-objects.py
@@ -0,0 +1,18 @@
+# Complete implementation of three methods
+# 
+#  __init__ should take an argument "name". It should initialize self.name to the argument and self.items to be an empty list
+#
+# add_item should append a dictionary representing an item to the store's items property. The dictionary should have keys name and price.
+#
+# stock_price should add up each item price inside self.items to come up with a total and return that
+
+class Store:
+    def __init__(self):
+        pass
+
+    def add_item(self, name, price):
+        pass
+
+    def stock_price(self):
+        pass
+
